b9ffb6a2552251a1071185fcc003028aa800e9c0
eliyash <eliyash3@gmail.com>
2015-10-19 01:36:10 +0300

    remove label after read, make send and read functions, add lines to readme

--- readMessage.py
+++ readMessage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# from getpass         import getpass
 import sys
 import imaplib
 import json
@@ -11,68 +10,64 @@ import json
 # import getpass
 
 
-# if  len(sys.argv)<4:
-#     print("error: run 'python *.py <email> <password> <target-pc-id>' ")
-#     sys.exit(1)
-
 
 with open('prog_data.json') as data_file:
     data = json.load(data_file)
-login, password = data["email"] , data["password"]
-name_of_machin = data["this user"]
 
 
 
+def readMassage(data):
+    login, password = data["email"] , data["password"]
+    name_of_machin = data["this user"]
+
+    EMAIL_FOLDER = name_of_machin
 
 
+    M = imaplib.IMAP4_SSL('imap.gmail.com')
 
-EMAIL_FOLDER = name_of_machin
+    try:
+        # rv, data = M.login(login, getpass.getpass())
+        rv, data = M.login(login, password)
+    except imaplib.IMAP4.error:
+        print("LOGIN FAILED!!! ")
+        sys.exit(1)
 
+    # print(rv, data)
 
-def process_mailbox(M):
+    rv, mailboxes = M.list()
+    # if rv == 'OK':
+    #     print("Mailboxes:")
+    #     print(mailboxes)
 
-    rv, data = M.search(None,'ALL')#BCC', '"eliyashaddget@gmail.com"')
-    if rv != 'OK':
-        print("No messages found!")
-        return
-    for num in data[0].split():
-        rv, data = M.fetch(num, '(RFC822)')
+    rv, data = M.select(EMAIL_FOLDER)
+    if rv == 'OK':
+        # print( "Processing mailbox...\n")
+
+        rv, data = M.search(None,'ALL')
         if rv != 'OK':
-            print("ERROR getting message", num)
+            print("No messages found!")
             return
+        for num in data[0].split():
+            rv, data = M.fetch(num, '(RFC822)')
+            if rv != 'OK':
+                print("ERROR getting message", num)
+                return
 
-        # msg = email.message_from_string(data[0][1])
-        msg = data[0][1]
-        # decode = email.header.decode_header(msg['Data'])[0]
-        # data = unicode(decode[0])
-        print("Massage: %s"% msg[(msg.find("Data:")+5):])
+            # msg = email.message_from_string(data[0][1])
+            msg = data[0][1]
+            # decode = email.header.decode_header(msg['Data'])[0]
+            # data = unicode(decode[0])
+            print("Massage: %s"% msg[(msg.find("Data:")+5):])
 
-        M.store(num,'+X-GM-LABELS', '%s_received'%name_of_machin)
-        M.store(num,'-X-GM-LABELS', '%s'%name_of_machin)
+            # M.store(num,'-X-GM-LABELS', name_of_machin)
+            M.store(num, '+FLAGS', '\\Deleted')
+            M.store(num,'+X-GM-LABELS', '%s_received'%name_of_machin)
 
-M = imaplib.IMAP4_SSL('imap.gmail.com')
+        M.expunge()  # not need if auto-expunge enabled
+        M.close()
+    else:
+        print("ERROR: Unable to open mailbox ", rv)
 
-try:
-    # rv, data = M.login(login, getpass.getpass())
-    rv, data = M.login(login, password)
-except imaplib.IMAP4.error:
-    print("LOGIN FAILED!!! ")
-    sys.exit(1)
 
-# print(rv, data)
 
-rv, mailboxes = M.list()
-# if rv == 'OK':
-#     print("Mailboxes:")
-#     print(mailboxes)
-
-rv, data = M.select(EMAIL_FOLDER)
-if rv == 'OK':
-    # print( "Processing mailbox...\n")
-    process_mailbox(M)
-    # M.select('[Gmail]/Trash')  # select all trash
-    # M.store("1:*", '+FLAGS', '\\Deleted')  #Flag all Trash as Deleted
-    M.expunge()  # not need if auto-expunge enabled
-    M.close()
-else:
-    print("ERROR: Unable to open mailbox ", rv)
+readMassage(data)
--- sendMessage.py
+++ sendMessage.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# from getpass         import getpass
 import smtplib
 import sys
 import json
 
-
 with open('prog_data.json') as data_file:
     data = json.load(data_file)
 
-login, password = data["email"] , data["password"]
-name_of_machin = data["other user 0"]
 
-fromaddr = '%s@gmail.com'%login
-toaddrs  = '%s+%s@gmail.com'%(login,name_of_machin)
-msg = 'Data:'+raw_input("massage:")
-username = fromaddr
-password = password
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.starttls()
-server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg)
-server.quit()
+def sendMassage(massage,data):
+    login, password = data["email"] , data["password"]
+    name_of_machin = data["other user 0"]
+
+    fromaddr = '%s@gmail.com'%login
+    toaddrs  = '%s+%s@gmail.com'%(login,name_of_machin)
+    msg = 'Data:'+massage
+    username = fromaddr
+    password = password
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+
+
+sendMassage(raw_input("massage:"),data)
